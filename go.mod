1828211a573cdcf8dcad44f62a9dc31d4933abdd
dimon5360 <dimon5360@gmail.com>
2022-08-01 00:30:56 +0300

    start web server with api and routing

--- go.mod
+++ go.mod
@@ -3,13 +3,18 @@ module app/main
 go 1.18
 
 require (
+	github.com/gin-gonic/gin v1.8.1
+	google.golang.org/grpc v1.46.0
+)
+
+require (
 	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -21,6 +26,7 @@ require (
 	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
 	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220503193339-ba3ae3f07e29 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -1,70 +1,85 @@
 package server
 
 import (
-	"app/main/kafka"
+	"app/main/postgres"
 	"app/main/utils"
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-type Ingredient struct {
-	Id    int32  `json:"id"`
-	Name  string `json:"name"`
-	Count int32  `json:"count"`
-}
-
-type Drink struct {
-	Id          int32  `json:"id"`
-	Title       string `json:"title"`
-	Price       int32  `json:"price"`
-	Description string `json:"description"`
-	Type        int32  `json:"type"`
-	// Ingredient  []Ingredient `json:"ingredient"`
-}
-
-// **************** API:
-// GET /drinks
-// GET /drinks/<id>
-// POST /drinks
-// DELETE /drinks/<id>
-// UPDATE /drinks/<id>
-
-var drinks = []Drink{
-	{Id: 1, Title: "Drink1", Price: 5699, Description: "Alcohol drink 1", Type: 1},
-	{Id: 2, Title: "Drink2", Price: 1799, Description: "Alcohol drink 1", Type: 1},
-	{Id: 3, Title: "Drink3", Price: 3999, Description: "Alcohol drink 1", Type: 1},
-}
-
-func GetDrinks(ctx *gin.Context) {
-
-	ctx.IndentedJSON(http.StatusOK, drinks)
-}
-
-type ServerConfig struct {
+type appConfig struct {
 	Host string `json:"host"`
 	Port uint16 `json:"port"`
 
-	kafkaHandler *kafka.Handler
+	DataServiceHost string `json:"data_access_service_host"`
+	DataServicePort uint16 `json:"data_access_service_port"`
+
+	dataService postgres.BarMapServiceClient
 }
 
-func SetupRouting(router *gin.Engine) {
-	router.GET("/drinks", GetDrinks)
-	// todo
+func SetupRouting(router *gin.Engine, app *appConfig) {
+
+	router.POST("/createbar", app.CreateBar)
+}
+
+// curl -i -X POST -H 'Content-Type: application/json'
+// -d '{"id": "1234", "title": "Goodson", "address": "Zelenograd",
+// "description": "bar"}' http://localhost:40400/createbar
+
+func (app *appConfig) CreateBar(c *gin.Context) {
+
+	log.Println("Create bar called")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	id := c.Query("id")
+	title := c.DefaultQuery("title", "Unknown")
+	address := c.PostForm("address")
+	description := c.PostForm("description")
+
+	r, err := app.dataService.CreateBar(ctx, &postgres.CreateBarRequest{
+		Id:          id,
+		Title:       title,
+		Address:     address,
+		Description: description,
+		Drinks:      []*postgres.CreateDrinkRequest{},
+	})
+
+	if err != nil {
+		log.Fatalf("could not create bar: %v", err)
+		c.String(http.StatusInternalServerError, "Creating bar failed")
+	}
+	c.String(http.StatusCreated, r.String())
+}
+
+func InitDataServiceGrpcConnection(app *appConfig) postgres.BarMapServiceClient {
+
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", app.DataServiceHost, app.DataServicePort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	return postgres.NewBarMapServiceClient(conn)
 }
 
 func Start(confFileName string) {
 
-	var config ServerConfig
-	utils.ParseJsonConfig(confFileName, &config)
+	var app appConfig
+	utils.ParseJsonConfig(confFileName, &app)
 
 	router := gin.Default()
-	SetupRouting(router)
-	go func() { router.Run(fmt.Sprintf("%s:%d", config.Host, config.Port)) }()
 
-	config.kafkaHandler = kafka.InitKafkaHandler("config/kafka.json")
+	app.dataService = InitDataServiceGrpcConnection(&app)
 
-	config.kafkaHandler.StartKafkaHandler()
+	SetupRouting(router, &app)
 
+	router.Run(fmt.Sprintf("%s:%d", app.Host, app.Port))
 }
